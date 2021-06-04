bea9501e00ae199afa88244f72160077c6d7bcb5
Omar Alhazmi <omaralhazmi@Omars-MacBook-Pro.local>
2021-06-04 18:51:36 +0300

    fix res in menu

--- src/components/hed_foot/Heder.js
+++ src/components/hed_foot/Heder.js
@@ -1,41 +1,39 @@
 import React, { Component } from 'react'
-import   '../styles/heder.css';
+import '../styles/heder.css';
 import logo from '../../image/logo.ico';
 export default class Heder extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-           toggle: false,
-        };
-    }
-    togglehandler() {
-        this.setState({
-           toggle: !this.state.toggle
-        })
-     }
-    render() {
-         const toggle = this.state.toggle
-        return (
-            <div>
-<div className ={toggle == true ? "responsive topnav" : "topnav" }>
-       <div className="logoContainer"> <img src={logo} class="logo" /></div>
-  <a href="#home" className="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <div className="dropdown">
-    <button className="dropbtn">Dropdown 
-      <i className="fa fa-caret-down"></i>
-    </button>
-    <div className="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
-  <a href="#about">About</a>
-  <a  className="icon" onClick={()=>this.togglehandler()} >&#9776;</a>
-</div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      toggle: false,
+    };
+  }
+  togglehandler() {
+    this.setState({
+      toggle: !this.state.toggle
+    })
+  }
+  render() {
+    const toggle = this.state.toggle
+    return (
+      <div>
+        <div className={toggle == true ? "responsive topnav" : "topnav"}>
+          <div className="logoContainer"> <img src={logo} class="logo" /></div>
+          <div className="barContent">
+            <a href="#home" className="active">الرئيسية</a>
+            <a href="#news">عن الجمعية</a>
+            <a href="#contact">الاخبار</a>
+            <a href="#contact">الفرق التطوعية</a>
+            <a href="#contact">السياسات والحوكمة</a>
+            <div className="dropdown">
+              <button className="dropbtn">تسجيل الدخول
+                <i className="fa fa-caret-down"></i>
+              </button>
             </div>
-        )
-    }
+          </div>
+          <a className="icon" onClick={() => this.togglehandler()} >&#9776;</a>
+        </div>
+      </div>
+    )
+  }
 }
