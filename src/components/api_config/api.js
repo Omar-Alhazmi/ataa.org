9e73a1aff73b9e2930992e69aaa1245a46968b9c
Omar Alhazmi <omaralhazmi@Omars-MacBook-Pro.local>
2021-08-04 22:58:03 +0300

    fix UserRegToTeam api methode

--- src/components/api_config/api.js
+++ src/components/api_config/api.js
@@ -1,13 +1,13 @@
-import apiURL from'./ApiConfig';
+import apiURL from './ApiConfig';
 import axios from 'axios';
-import {checkStorage} from '../helperMethods';
+import { checkStorage } from '../helperMethods';
 //================== Helper Method ============================||
-const config ={
-    headers:{}
-  }
-  if(checkStorage()){
-    config.headers['Authorization'] = `Bearer ${checkStorage()}`
-  }
+const config = {
+  headers: {}
+}
+if (checkStorage()) {
+  config.headers['Authorization'] = `Bearer ${checkStorage()}`
+}
 
 //================== Helper Method ============================||
 //---------------All POST Request-------------------//
@@ -16,64 +16,68 @@ export const UserRegistration = req => {
   return axios({
     method: 'POST',
     url: apiURL + 'api/User/register',
-    data:{
+    data: {
       FullName: req.FullName,
       UserName: req.UserName,
       password: req.password,
-      Email:req.Email,
+      Email: req.Email,
       Phone: req.Phone,
       Role: req.Role,
       NationalId: req.NationalId,
-      Job:req.Job,
-      Education:req.Education,
-      Gender:req.Gender
-   }
+      Job: req.Job,
+      Education: req.Education,
+      Gender: req.Gender
+    }
   })
 }
 //========================= =============================\\
-export const TeamRegistration = (req,id,Logo) => {
+export const TeamRegistration = (req, id, Logo) => {
   const formData = new FormData();
   Object.entries(req).forEach(([key, value]) => {
-     formData.append(key, value)}) 
-     formData.append("Logo", Logo);
-   return axios.post(`${apiURL}api/request/register/new/Team/${id}`,formData,config)
-   .then(res => {
-     console.log(res);
-    localStorage.setItem("leadAt", res.data.token)
-     window.location.reload(false)
-   }
+    formData.append(key, value)
+  })
+  formData.append("Logo", Logo);
+  return axios.post(`${apiURL}api/request/register/new/Team/${id}`, formData, config)
+    .then(res => {
+      console.log(res);
+      localStorage.setItem("leadAt", res.data.token)
+      window.location.reload(false)
+    }
     )
-       .catch(err => console.log(err));
- }; 
+    .catch(err => console.log(err));
+};
 //========================= =============================\\
-export const UpdateTeam = (req,id,Logo) => {
-   const formData = new FormData();
-      Object.entries(req).forEach(([key, value]) => {
-         formData.append(key, value)}) 
-         formData.append("Logo", Logo);
-       return axios.patch(`${apiURL}api/Update/TeamBy/${id}`,formData,config)
-       .then(res =>
-        window.location.reload(false)
-        )
-           .catch(err => console.log(err));
-     }; 
+export const UpdateTeam = (req, id, Logo) => {
+  const formData = new FormData();
+  Object.entries(req).forEach(([key, value]) => {
+    formData.append(key, value)
+  })
+  formData.append("Logo", Logo);
+  return axios.patch(`${apiURL}api/Update/TeamBy/${id}`, formData, config)
+    .then(res =>
+      window.location.reload(false)
+    )
+    .catch(err => console.log(err));
+};
 //=====================  =================
-export const UserRegToTeam = (userId,teamId) => {
-  req.Members
-      return axios.patch(`${apiURL}/api/PassTeam/${teamId}`,userId,config)
-      .then(res =>
-       window.location.reload(false)
-       )
-        .catch(err => console.log(err));
-    }; 
+export const UserRegToTeam = (userId, teamId) => {
+  return axios({
+    method: 'patch',
+    url: `${apiURL}api/PassTeam/${teamId}`,
+    data: {
+      Members: userId,
+    },
+    config
+  })
+}
 //---------------All GET Request-------------------//
-export const getAllNews = () =>{
-    return axios.get(`${apiURL}api/get/All/News`);
-  }  
-export const getAllTeams = () =>{
-    return axios.get(`${apiURL}api/get/allTeams`);
-  }
-  export const getTeamLeader = (id) =>{
-    return axios.get(`${apiURL}api/get/Team/ByUser/${id}`,config);
-  }  
+export const getAllNews = () => {
+  return axios.get(`${apiURL}api/get/All/News`);
+}
+export const getAllTeams = () => {
+  return axios.get(`${apiURL}api/get/allTeams`);
+}
+export const getTeamLeader = (id) => {
+  return axios.get(`${apiURL}api/get/Team/ByUser/${id}`, config);
+}
 
