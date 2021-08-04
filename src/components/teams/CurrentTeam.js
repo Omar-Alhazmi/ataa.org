cf5e24c9ac4c7de5b49b4be4ecc7d687eac5739d
Omar Alhazmi <omaralhazmi@Omars-MacBook-Pro.local>
2021-08-04 23:00:50 +0300

    implment UserRegToTeam  ruote

--- src/components/teams/CurrentTeam.js
+++ src/components/teams/CurrentTeam.js
@@ -1,43 +1,62 @@
 import React, { Component } from 'react'
 import { BsArrowRight } from "react-icons/bs";
 import apiURL from '../api_config/ApiConfig';
-import {UserRegToTeam} from '../api_config/api';
-import {getId} from '../helperMethods';
+import { UserRegToTeam } from '../api_config/api';
+import { getId , checkStorage, getInfo } from '../helperMethods';
 import "../styles/TeamLeaderLayout.css"
 import Swal from "sweetalert2";
-
 export default class currentTeam extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            teamId: this.props.id
+            teamId: this.props.id,
+            alreadyRetested: false
+
         };
     }
 
-handleSubmit(){
-            const  userId = getId()
-            const teamId = this.props.data._id
-            const teamName =  this.props.data.data.TeamName
-        UserRegToTeam(userId,teamId)
-        .then(response => {
-            try {
-                console.log(response);
-                Swal.fire({
-                    title: `  تم التسجيل في  فريق    ${teamName}   بنجاح`,
-                    icon: 'success',
-                    confirmButtonText: 'موافق',
-                    showCancelButton: false,
-                })
-            }
-            catch (error) {
-                Swal.fire({
-                    title: ` ${response.data.message}`,
-                    icon: 'error',
-                    showCancelButton: false,
-                })
-            }
-        })
+    handleSubmit() {
+        const userId = getId()
+        const teamId = this.props.data._id
+        const teamName = this.props.data.data.TeamName
+        let alreadyRetested = false
+        const Retested = this.props.data.Members.filter((Retested => {
+            return Retested._id === userId
+        }))
+        if (Retested === undefined || Retested === null || Retested.length !== 0) {
+            console.log(Retested);
+            alreadyRetested = true
         }
+        if (alreadyRetested === true) {
+            Swal.fire({
+                title: ` تم السجيل في الفريق من قبل`,
+                icon: 'error',
+                showCancelButton: false,
+            })
+        }
+        else if (alreadyRetested === false) {
+            UserRegToTeam(userId, teamId)
+                .then(response => {
+                    try {
+                        Swal.fire({
+                            title: `  تم التسجيل في  فريق    ${teamName}   بنجاح`,
+                            icon: 'success',
+                            confirmButtonText: 'موافق',
+                            showCancelButton: false,
+                        })
+                        window.location.reload(false);
+                    }
+                    catch (error) {
+                        Swal.fire({
+                            title: ` ${response.data.message}`,
+                            icon: 'error',
+                            showCancelButton: false,
+                        })
+                    }
+                })
+        }
+    }
+    
     render() {
         const { Leader, Members, Logo } = this.props.data
         const { CreateAt, GeneralGoal, Message, NumberOfII, SpecificGoal, TeamName, Vision } = this.props.data.data
@@ -45,8 +64,9 @@ handleSubmit(){
             <>
                 <div>
                     <button className="button" onClick={e => this.props.tog(e)}> <BsArrowRight /></button>
+                   {checkStorage() !== null && getInfo().data.Role !== "TeamLeader" ?
                     <button className="button" onClick={e => this.handleSubmit(e)}> التسجيل في الفريق</button>
-
+                        :""}
                     <div className="teamContainer">
                         <div className="teamLogo">
                             <img src={`${apiURL}${Logo}`} alt="logo" />
@@ -91,10 +111,6 @@ handleSubmit(){
                 <h2 className="heading_2">
                     اعضاء الفريق : {Members.length}
                 </h2>
-                {/* <h2 className="heading_2">
-                        {Members.length}
-                    </h2> */}
-
                 <div className="crew-members">
                     <div className="member">
                         قائد الفريق
@@ -102,49 +118,18 @@ handleSubmit(){
                             {Leader.FullName}
                         </p>
                     </div>
-                    <div className="member">
-                        قائد الفريق
-                        <p className="discretion">
-                            {Leader.FullName}
-                        </p>
-                    </div>
-                    <div className="member">
-                        قائد الفريق
-                        <p className="discretion">
-                            {Leader.FullName}
-                        </p>
-                    </div>
-                    <div className="member">
-                        قائد الفريق
-                        <p className="discretion">
-                            {Leader.FullName}
-                        </p>
-                    </div>
-
-                    <div className="member">
-                        قائد الفريق
-                        <p className="discretion">
-                            {Leader.FullName}
-                        </p>
-                    </div>
-                    <div className="member">
-                        قائد الفريق
-                        <p className="discretion">
-                            {Leader.FullName}
-                        </p>
-                    </div>
-                    <div className="member">
-                        قائد الفريق
-                        <p className="discretion">
-                            {Leader.FullName}
-                        </p>
-                    </div>
-                    <div className="member">
-                        قائد الفريق
-                        <p className="discretion">
-                            {Leader.FullName}
-                        </p>
-                    </div>
+                    {
+                        Members.map((item, index) => {
+                            return (
+                                <div className="member" key={index}>
+                                    عضو الفريق
+                                    <p className="discretion">
+                                        {item.FullName}
+                                    </p>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </>
         )
